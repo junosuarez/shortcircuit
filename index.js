46b9a49991904b461eeeed21d14eedb2621dc599
Patrik Johnson <patrik@zebros.fi>
2014-02-26 15:19:04 +0200

    Added closing semicolons to remove jshint warnings

--- index.js
+++ index.js
@@ -1,87 +1,87 @@
-var Promise = require('bluebird')
-var slice = Array.prototype.slice
+var Promise = require('bluebird');
+var slice = Array.prototype.slice;
 
 function isPromise(p) {
-  return p && typeof p.then === 'function'
+  return p && typeof p.then === 'function';
 }
 
 function pcall (p) {
-  var t = typeof p
-  if (t === 'boolean') return Promise.cast(p)
-  if (t === 'function') return pcall(p())
-  if (isPromise(p)) return p
-  return Promise.resolve(Boolean(p))
+  var t = typeof p;
+  if (t === 'boolean') return Promise.cast(p);
+  if (t === 'function') return pcall(p());
+  if (isPromise(p)) return p;
+  return Promise.resolve(Boolean(p));
 }
 
 function or () {
   var args = arguments;
-  var term = args[0]
+  var term = args[0];
   if (!term) return Promise.reject(new Error('No terms'));
   if (args.length === 1) {
-    return pcall(term).then(Boolean)
+    return pcall(term).then(Boolean);
   }
 
   return pcall(term).then(function (val) {
     if (val) return true;
     // async recurse
-    return or.apply(null, slice.call(args, 1))
-  })
+    return or.apply(null, slice.call(args, 1));
+  });
 }
 
 function and () {
   var args = arguments;
-  var term = args[0]
-  if (!term) return Promise.reject(new Error('No terms'))
+  var term = args[0];
+  if (!term) return Promise.reject(new Error('No terms'));
   if (args.length === 1) {
-    return pcall(term).then(Boolean)
+    return pcall(term).then(Boolean);
   }
 
   return pcall(term).then(function (val) {
     if (!val) return false;
     // asycn recurse
-    return and.apply(null, slice.call(args, 1))
-  })
+    return and.apply(null, slice.call(args, 1));
+  });
 }
 
 
 function some () {
   var args = arguments;
-  var term = args[0]
-  if (!term) return Promise.reject(new Error('No terms'))
+  var term = args[0];
+  if (!term) return Promise.reject(new Error('No terms'));
   if (args.length === 1) {
-    return pcall(term).then(Boolean)
+    return pcall(term).then(Boolean);
   }
 
-  var promises = slice.call(arguments).map(pcall)
+  var promises = slice.call(arguments).map(pcall);
 
   return Promise.all(promises).then(function (results) {
-    return results.some(Boolean)
-  })
+    return results.some(Boolean);
+  });
 }
 
 function every () {
   var args = arguments;
-  var term = args[0]
-  if (!term) return Promise.reject(new Error('No terms'))
+  var term = args[0];
+  if (!term) return Promise.reject(new Error('No terms'));
   if (args.length === 1) {
-    return pcall(term).then(Boolean)
+    return pcall(term).then(Boolean);
   }
 
-  var promises = slice.call(arguments).map(pcall)
+  var promises = slice.call(arguments).map(pcall);
 
   return Promise.all(promises).then(function (results) {
-    return results.every(Boolean)
-  })
+    return results.every(Boolean);
+  });
 }
 
 
 function not(term) {
-  if (!term) return Promise.reject(new Error('No term'))
-  return pcall(term).then(function (x) { return !x })
+  if (!term) return Promise.reject(new Error('No term'));
+  return pcall(term).then(function (x) { return !x; });
 }
 
-module.exports.or = or
-module.exports.and = and
-module.exports.not = not
-module.exports.every= every
-module.exports.some = some
+module.exports.or = or;
+module.exports.and = and;
+module.exports.not = not;
+module.exports.every = every;
+module.exports.some = some;
